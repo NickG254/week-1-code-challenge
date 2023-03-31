c0b4f13a5ae1ab5c66670bdefbd3c5b7f3401eb6
NickG254 <126749092+NickG254@users.noreply.github.com>
2023-03-31 09:14:25 +0300

    Add files via upload

---
+++ challenge-1.js
@@ -0,0 +1,21 @@
+let marks = prompt("Enter student marks:");
+let Grades;
+
+if (marks < 0 || marks > 100) {
+  console.log("Invalid input!");
+} else {
+ 
+  if (marks >= 80) {
+    Grades = "A";
+  } else if (marks >= 60 && marks < 80) {
+    Grades = "B";
+  } else if (marks >= 50 && marks < 60) {
+    Grades = "C";
+  } else if (marks >= 40 && marks < 50) {
+    Grades = "D";
+  } else {
+    Grades = "E";
+  }
+}
+
+console.log(Grades);
---
+++ challenge-2.js
@@ -0,0 +1,16 @@
+function calculatePoints(speed) {
+    
+    const speedLimit = 70;
+    const kmPerreducePoint = 5;
+    
+    if (speed <= speedLimit) {
+      console.log("Okay");
+    } else {
+      demeritPoints = Math.floor((speed - speedLimit) / kmPerreducePoint);
+      console.log(`Points: ${demeritPoints}`);
+      if (demeritPoints >= 12) {
+        console.log("License suspended");
+      }
+    }
+  }
+  calculatePoints(70);
